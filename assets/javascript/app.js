8c9f4dafffcd9eb8bbb412eecb9454753e42e8c2
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2017-11-26 21:40:29 -0800

    Added games array

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+//alert("Developed by Stephen Klein");
+
+// Section 1: Global Variables
+//=========================================================
+// Arryas and variables for holding data.
+var gameOptions = ["battlefield", "portal", "minecraft", "bioshock", "doom", "destiny", "quake", "fallout", "burnout", "fear"]
+
+
+console.log(gameOptions);
+// Section 2: Functions
+//=========================================================
+
+
+
+
+
+// Section 3: Main Process
+//=========================================================
