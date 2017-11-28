f224fc999971b7aaed5ed4b5ca8397007b9e7aa0
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2017-11-27 21:35:19 -0800

    injected Javascript into html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,9 @@
 // Section 1: Global Variables
 //=========================================================
 // Arryas and variables for holding data.
-var wordOptions = ["battlefield", "portal", "minecraft", "bioshock", "doom", "destiny", "quake", "fallout", "burnout", "fear"];
+var wordOptions = ["battlefield", "portal", "minecraft", "bioshock", "doom", "destiny", "quake", "fallout", "burnout", "fear", "forza"];
 var selectedWord = "";
-var lattersInWord = [];
+var lettersInWord = [];
 var numBlanks =0;
 var blanksAndSuccesses = [];
 var wrongLetters =[];
@@ -21,13 +21,38 @@ var guessesLeft =9;
 	
 function startGame() {
 	selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+	lettersInWord = selectedWord.split("");
+	numBlanks = lettersInWord.length;
+	
 
+	// reset
+	guessesLeft = 9;
+	wrongLetters = [];
+	blanksAndSuccesses = [];
+
+	// populate blanks and successes with right number of blanks.
+	for (var i = 0; i < numBlanks; i++){
+		blanksAndSuccesses.push("_");
+	}
+
+	// Change HTML to reflect round conditions
+	document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses.join(" ");
+	document.getElementById('numGuesses').innerHTML = guessesLeft;
+	document.getElementById('winCounter').innerHTML = winCount;
+	document.getElementById('lossCounter').innerHTML = loseCount;
 	// Testing/ Debuggingdf
 	 console.log(selectedWord);
+	 console.log(lettersInWord);
+	 console.log(numBlanks);
+	 console.log(blanksAndSuccesses);
 }
  
-
+	
 
 // Section 3: Main Process
 //=========================================================
+
+// Initiates the code the first time.
 startGame();
+
+//  Register keyclicks
