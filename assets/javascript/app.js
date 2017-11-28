5d23767726ac282a98509dc541020e77e02b42e8
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2017-11-27 20:18:52 -0800

    Added global variables and start function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,16 +3,31 @@
 // Section 1: Global Variables
 //=========================================================
 // Arryas and variables for holding data.
-var gameOptions = ["battlefield", "portal", "minecraft", "bioshock", "doom", "destiny", "quake", "fallout", "burnout", "fear"]
+var wordOptions = ["battlefield", "portal", "minecraft", "bioshock", "doom", "destiny", "quake", "fallout", "burnout", "fear"];
+var selectedWord = "";
+var lattersInWord = [];
+var numBlanks =0;
+var blanksAndSuccesses = [];
+var wrongLetters =[];
+
+// game counters
+var winCount = 0;
+var loseCount = 0;
+var guessesLeft =9;
 
 
-console.log(gameOptions);
 // Section 2: Functions
 //=========================================================
+	
+function startGame() {
+	selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
 
-
-
+	// Testing/ Debuggingdf
+	 console.log(selectedWord);
+}
+ 
 
 
 // Section 3: Main Process
 //=========================================================
+startGame();
